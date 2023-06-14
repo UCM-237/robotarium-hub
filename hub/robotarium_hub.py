b6991e90f448a5147f463a3a2e9cd11b21e867fe
Jesús Chacón <jcsombria@gmail.com>
2023-06-14 17:21:53 +0200

    Add MQTT integration

--- hub/robotarium_hub.py
+++ hub/robotarium_hub.py
@@ -3,7 +3,9 @@
 import logging
 import zmq
 import json
+import random
 
+from paho.mqtt import client as mqtt_client
 from time import sleep
 from threading import Thread
 
@@ -12,6 +14,16 @@ logging.basicConfig(level=logging.DEBUG)
 
 CMD_PORT = 5555
 DATA_PORT = 5556
+
+# MQTT Config
+MQTT_BROKER = '127.0.0.1'
+MQTT_PORT = 1883
+MQTT_TOPIC = "#"
+# Generate a Client ID with the publish prefix.
+MQTT_CLIENT_ID = f'publish-{random.randint(0, 1000)}'
+# MQTT_USERNAME = 'emqx'
+# MQTT_PASSWORD = 'public'
+
 
 class RobotariumHub:
   def __init__(self):
@@ -30,6 +42,27 @@ class RobotariumHub:
     self.poller = zmq.Poller()
     self.listening = Thread(target=self.listen, args=())
     self.listening.start()
+    self.mqtt_client = self.connect_mqtt()
+
+  def connect_mqtt(self):
+    def on_connect(client, userdata, flags, rc):
+      if rc == 0:
+        print("Connected to MQTT Broker!")
+      else:
+        print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(MQTT_CLIENT_ID)
+    # client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    client.on_connect = on_connect
+    client.connect(MQTT_BROKER, MQTT_PORT)
+    client.loop_start()
+    client.subscribe('#')
+    client.on_message = self.on_mqtt_message
+    return client
+
+  def on_mqtt_message(self, client, userdate, msg):
+    self.data_socket.send_string(msg.topic, flags=zmq.SNDMORE)
+    self.data_socket.send_string(msg.payload.decode())
 
   def add_agent(self, id, url):
     logging.info(f'Agent {id} registered with url {url}');
@@ -40,6 +73,7 @@ class RobotariumHub:
     self.agents[id]['socket'].connect(url)
     self.agents[id]['socket'].subscribe('')
     self.poller.register(self.agents[id]['socket'], zmq.POLLIN)
+
 
   def listen(self):
     logging.info(f'Listening to agents')
@@ -64,6 +98,10 @@ class RobotariumHub:
           else:
             self.data_socket.send_string(topic, flags=zmq.SNDMORE)
             self.data_socket.send_json(message)
+          
+          self.mqtt_client.publish(topic, json.dumps(message))
+
+
 
   def accept(self):
     '''Wait for next request from client'''
@@ -76,5 +114,15 @@ class RobotariumHub:
 
 
 
+
+
+def subscribe(client: mqtt_client):
+  def on_message(client, userdata, msg):
+    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+  client.subscribe(topic)
+  client.on_message = on_message
+
+
 if __name__ == "__main__":
-    hub = RobotariumHub()
+  hub = RobotariumHub()
