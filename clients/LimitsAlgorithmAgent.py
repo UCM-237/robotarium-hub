4ad3e6bf16b095dbfd387cc39c6d8ac0d26ea5ab
truposky <weselguti@gmail.com>
2024-02-23 12:03:37 +0100

    LimitsAlgorithmAgent

--- clients/LimitsAlgorithmAgent.py
+++ clients/LimitsAlgorithmAgent.py
@@ -12,6 +12,8 @@ import numpy as np
 
 from agent import Agent
 
+from OrientationControl import orientationControl
+
 # Configure logs
 logging.basicConfig(level=logging.INFO)
 
@@ -20,7 +22,7 @@ AGENT_ID = 'LimitsAlgorithm'
 AGENT_IP = '127.0.0.1'
 AGENT_CMD_PORT = 5563
 AGENT_DATA_PORT = 5564
-AGENT_ID_NUM= '0'
+AGENT_ID_NUM= '2'
 # Where the server is 
 HUB_IP = '127.0.0.1'
 HUB_CMD_PORT = 5555
@@ -39,16 +41,26 @@ class Algorithm:
   def __init__(self, agent: Agent) -> None:
     self.agent = agent
     self.Position={}
+    self.PostionForControl={}
     self.L = 12.4  # Valor de ejemplo para L
     self.R = 3.35  # Valor de ejemplo para R
     self.A = [[self.L/(2*self.R), 1/self.R],
         [-self.L/(2*self.R), 1/self.R]]
     self.ArenaLimits = {}
+    self.OrientationControl = orientationControl()
+    self.orientationCorrected = False
+    self.newHeadingRequired = False
+    self.newHeading=0
+    self.SAMPLETIME=200
+    self.tval_before = 0
+    self.tval_after= 0
+    self.tval_sample = 0
 
   def connect(self):
     logging.debug('starting device')
    
     self.thread = Thread(target=self.Algorithm).start()
+    self.thread = Thread(target=self.correctOrientation).start()
     
   
   def Algorithm(self):
@@ -56,45 +68,51 @@ class Algorithm:
    
     while(1):
       self.direction()
+
   def direction(self):
     minimumSafetyDistance=10
+    w=0
+    vel=0
+    angularWheel = [0.0, 0.0]
+    
     while (AGENT_ID_NUM not in self.Position) or len(self.ArenaLimits)==0:
         if(len(self.ArenaLimits)==0):
             self.agent.send("localization/RobotariumData","")
             time.sleep(1)
-        position=self.Position
-        robotData = json.loads(position[AGENT_ID_NUM])
-        position.pop(AGENT_ID_NUM)
-        robotX = float(robotData["x"])
-        robotY = float(robotData["y"])
-        heading = -float(robotData["yaw"])
-        newHeading = 0
-        dx = math.cos(heading)
-        if dx < 0.08 and dx > -0.08:
-            dx = 0
-        dy = math.sin(heading)
-        if dy < 0.08 and dy > -0.08:
-            dy = 0
+    position=self.Position
+    robotData = json.loads(position[AGENT_ID_NUM])
+    position.pop(AGENT_ID_NUM)
+    robotX = float(robotData["x"])
+    robotY = float(robotData["y"])
+    heading = -float(robotData["yaw"])
+    self.newHeading = 0
+    dx = math.cos(heading)
+    if dx < 0.08 and dx > -0.08:
+        dx = 0
+    dy = math.sin(heading)
+    if dy < 0.08 and dy > -0.08:
+        dy = 0
 
-        # coordinates of the rectangle
-        x1=self.ArenaLimits["x1"]
-        y1=self.ArenaLimits["y1"] 
-        x2=self.ArenaLimits["x2"]
-        y2=self.ArenaLimits["y2"]
-        x3=self.ArenaLimits["x3"]
-        y3=self.ArenaLimits["y3"]
-        x4=self.ArenaLimits["x4"]
-        y4=self.ArenaLimits["y4"]
+    # coordinates of the rectangle
+    x1=self.ArenaLimits["x1"]
+    y1=self.ArenaLimits["y1"] 
+    x2=self.ArenaLimits["x2"]
+    y2=self.ArenaLimits["y2"]
+    x3=self.ArenaLimits["x3"]
+    y3=self.ArenaLimits["y3"]
+    x4=self.ArenaLimits["x4"]
+    y4=self.ArenaLimits["y4"]
 
-        segmentOfTheRectangle =[                                  #S1       
-            segment((x1,y1),(x2,y2)),#segment 1              -------------------
-            segment((x2,y2),(x3,y3)),#segment 2              |                 |         
-            segment((x3,y3),(x4,y4)),#segment 3          s4  |                 | #s2
-            segment((x4,y4),(x1,y1))  #segment 4             |                 |
-                                                            #------------------- 
-        ]                                                       #s3
-        d=0 #distance to the nearest wall
-        # Comprobar la dirección
+    segmentOfTheRectangle =[                                  #S1       
+        segment((x1,y1),(x2,y2)),#segment 1              -------------------
+        segment((x2,y2),(x3,y3)),#segment 2              |                 |         
+        segment((x3,y3),(x4,y4)),#segment 3          s4  |                 | #s2
+        segment((x4,y4),(x1,y1))  #segment 4             |                 |
+                                                        #------------------- 
+    ]                                                       #s3
+    d=0 #distance to the nearest wall
+    # Comprobar la dirección
+    if self.newHeadingRequired == False:
         if dx > 0:
             if dy > 0:
                 print('The robot is moving to the upper righ')
@@ -108,9 +126,9 @@ class Algorithm:
                     d = min(d1, d2)
                     if d<minimumSafetyDistance:
                         if d == d1:
-                            newHeading=-heading
+                            self.newHeading=-heading
                         else:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                             
                 else:
                     #stop the robot
@@ -125,9 +143,9 @@ class Algorithm:
                     d = min(d1, d2)
                     if d<minimumSafetyDistance:
                         if d == d1:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                         else:
-                            newHeading = -heading   
+                            self.newHeading = -heading   
                 else:
                     #stop the robot
                     print("probably collision with the wall at right. TODO: STOP THE ROBOT and turn it to the right")
@@ -143,11 +161,11 @@ class Algorithm:
                     d = min(d1, d2, d3)
                     if d<minimumSafetyDistance:
                         if d == d1:
-                            newHeading = -heading
+                            self.newHeading = -heading
                         elif d == d2:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                         else:
-                            newHeading = -heading
+                            self.newHeading = -heading
         elif dx < 0:
             if dy > 0:
                 print('The robot is moving to the upper left corner.')
@@ -160,9 +178,9 @@ class Algorithm:
                     d = min(d1, d2)
                     if d<minimumSafetyDistance:
                         if d == d1:
-                            newHeading = -heading
+                            self.newHeading = -heading
                         else:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                 else:
                     #stop the robot
                     print("probably collision with the wall at left.TODO: STOP THE ROBOT and turn it to the right")
@@ -177,9 +195,9 @@ class Algorithm:
                     d = min(d1, d2)
                     if d<minimumSafetyDistance:
                         if d == d1:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                         else:
-                            newHeading = -heading
+                            self.newHeading = -heading
                 else:
                     #stop the robot
                     print("probably collision with the wall at left. TODO: STOP THE ROBOT and turn it to the right")
@@ -195,11 +213,11 @@ class Algorithm:
                     d = min(d1, d2, d3)
                     if d<minimumSafetyDistance:
                         if d == d1:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                         elif d == d2:
-                            newHeading = -heading
+                            self.newHeading = -heading
                         else:
-                            newHeading = -(math.pi - abs(heading))
+                            self.newHeading = -(math.pi - abs(heading))
                 else:
                     #stop the robot
                     print("probably collision with the wall at left. TODO : STOP THE ROBOT and turn it to the right")  
@@ -208,15 +226,63 @@ class Algorithm:
                 print('The robot is moving up.')
                 d = self.distance(robotX, robotY, segmentOfTheRectangle[0])
                 if d<minimumSafetyDistance:
-                    newHeading = -heading
+                    self.newHeading = -heading
             elif dy < 0:
                 print('The robot is moving down.')
                 d = self.distance(robotX, robotY, segmentOfTheRectangle[2])
                 if d<minimumSafetyDistance:
-                    newHeading = -(math.pi - abs(heading))
+                    self.newHeading = -(math.pi - abs(heading))
             else:
                 print('El robot no se está moviendo, O no se detecta')
-
+        
+        if self.newHeading != 0:
+            #stop robot
+            w=0
+            vel=0
+            velocity_robot=[w,vel]
+            self.angularWheelSpeed(angularWheel,velocity_robot)
+            self.agent.send('control/2/move',{'v_left':angularWheel[0],'v_right':angularWheel[1]})
+            time.sleep(1)
+            self.self.newHeadingRequired=True
+        
+            
+            
+  def correctOrientation(self):
+    vel=0
+    angularWheel = [0.0, 0.0]
+    while True:
+        if len(self.PostionForControl)>0:
+            
+            robotData = json.loads(self.PostionForControl[AGENT_ID_NUM])
+            self.PostionForControl.pop(AGENT_ID_NUM)
+            robotX = float(robotData["x"])
+            robotY = float(robotData["y"])
+            heading = -float(robotData["yaw"])
+        #now correct orientation 
+            if self.newHeadingRequired == True:
+                w =  orientationControl.PID(heading, self.newHeading)
+                if w !=0:
+                    velocity_robot=[w,vel]
+                    self.angularWheelSpeed(angularWheel,velocity_robot)
+                    self.agent.send('control/2/move',{'v_left':angularWheel[0],'v_right':angularWheel[1]})
+                else :
+                    self.newHeadingRequired = False
+                    self.Pos
+                    vel=8.2*3.35
+                    velocity_robot=[w,vel]
+                    self.angularWheelSpeed(angularWheel,velocity_robot)
+                    self.agent.send('control/2/move',{'v_left':angularWheel[0],'v_right':angularWheel[1]})
+        self.tval_after=time.time()*1000
+        self.tval_sample = self.tval_after-self.tval_before
+        if self.tval_sample < 0:
+            print("Error de tiempo")
+            print(self.tval_sample)
+        elif self.tval_sample > self.SAMPLETIME:
+            print("(movimiento) Tiempo del programa mayor: ", self.tval_sample)
+        else:
+            #print((self.SAMPLETIME - self.tval_sample) / 1000)
+            time.sleep((self.SAMPLETIME - self.tval_sample) / 1000)
+      
   def distance(self,x, y, seg):
     # Project the point onto the line that defines the segment (dot product)
     proy = np.dot(seg.pf - seg.pi, np.array([x, y]) - seg.pi) / np.linalg.norm(seg.pf - seg.pi)
@@ -247,6 +313,7 @@ class Algorithm:
     return d
   def stop(self):
     print("stop")
+    
   def angularWheelSpeed(self, w_wheel, velocity_robot):
     fila = 2
     columna = 2
@@ -281,6 +348,7 @@ class Algorithm:
     #register the position of the robot in a dictionary
     if topic == "position":
       self.Position[agent]=message
+      self.PostionForControl[agent]=message
       #print(message)
     elif topic == "ArenaSize":
       self.ArenaLimits = json.loads(message)
