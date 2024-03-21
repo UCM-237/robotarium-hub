0cc0dc12c9a5f085d154f58e00e673681af613fb
truposky <weselguti@gmail.com>
2024-03-21 22:15:44 +0100

    update

--- firmware/RobotSerialCommunication/arduinoAgent/operation.h
+++ firmware/RobotSerialCommunication/arduinoAgent/operation.h
@@ -2,18 +2,19 @@
 #define OPERATION_H
 
 
-enum Operation{
+typedef enum Operation{
   OP_ERROR = 99,
-  OP_SALUDO = 1,
-  OP_MOVE_WHEEL = 2,
-  OP_STOP_WHEEL = 3,
-  OP_VEL_ROBOT  = 5,
+  OP_HELLO = 1,
+  OP_MOVE_ROBOT = 2,
+  OP_STOP_ROBOT = 3,
+  OP_VEL_ROBOT  = 4,
   OP_POSITION   = 8,
   OP_CONF_PID   = 10,
+  OP_CONF_FF    = 11,//FeedForward
    
-};
+}operation_t;
 
 
-Operation operation;
+operation_t operation;
 
 #endif
