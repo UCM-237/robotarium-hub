0cfa54cf1482a72ff9eb87e33e4567e86e0920f0
Liagar <liagar@gmail.com>
2026-06-24 10:55:31 +0200

    Mejoras en los agentes pos y viksec para que no se pierdan mensajes en la recepcion

--- clients/PositionAgent/pos_agent.py
+++ clients/PositionAgent/pos_agent.py
@@ -87,12 +87,17 @@ class ArucoDevice:
         if topic == "vision/stitched":
             try:
                 if not self.image_queue.full():
-                    self.image_queue.put(message)
+                    self.image_queue.put_nowait(message)
                 else:
                     # Si el robot va lento procesando, descartamos el frame viejo
                     pass 
   
+            except queue.Full:
+                self.missed_frames += 1
+                if self.missed_frames % 10 == 0:
+                    logger.warning(f"[ADVERTENCIA] Se han perdido {self.missed_frames} frames por saturación de la cola.")
             except Exception as e:
+
                 logger.error(f"[ERROR] Error al procesar frame: {e}")
 
     def _image_processing_loop(self):
@@ -102,135 +107,127 @@ class ArucoDevice:
             try:
                 payload = self.image_queue.get()
                 # 1. Convertir el string JSON a diccionario
-                # NUEVA COMPROBACIÓN Y DECODIFICACIÓN SEGURA:
-                if payload is None:
-                    self.image_queue.task_done()
-                    logger.warning("Payload vacion")
-                    continue
-                    
-                if isinstance(payload, bytes):
-                    payload = payload.decode('utf-8')
-                    
-                payload = payload.strip() # Limpia espacios en blanco o saltos de línea
-                
-                if not payload:
-                    logger.warning("[ADVERTENCIA] Se recibió un payload vacío en la cola.")
+                try:
+                    data = json.loads(payload)
+                    img_b64=data.get("image")
+                except Exception as json_error:
+                    logger.error(f"[ERROR] Error al decodificar JSON: {payload}")
                     self.image_queue.task_done()
                     continue
-               
-                data = json.loads(payload)
-                
-                # 2. Extraer la imagen en Base64 y decodificarla
-                # Basado en el payload que envía tu vision_agent.py
-                img_b64 = data['image']
-                img_bytes = base64.b64decode(img_b64)
-                
-                # 3. Convertir bytes a imagen de OpenCV
-                np_array = np.frombuffer(img_bytes, dtype=np.uint8)
-                frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-                # Convertimos a gris
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-                gray=clahe.apply(gray)
-
-
-                # Aplicamos una ecualización de histograma para resaltar los bordes
-                # Esto ayuda mucho si la iluminación es pobre
-                #gray = cv2.equalizeHist(gray)
-                if frame is not None:
-                    # 2. DETECCIÓN DE ARUCOS
-                    # corners: lista de esquinas de los marcadores detectados
-                    # ids: identificadores de cada marcador
-                    #Justo antes de detectMarkers, aplica un umbral manual para testear
-                    # Esto te permitirá ver si el ArUco se está "emborronando"
-                    _, testing_thresh = cv2.threshold(gray, 150, 180, cv2.THRESH_BINARY)
-                    #cv2.imshow("Test Umbral", testing_thresh) # Si aquí el ArUco se ve todo negro o todo blanco, ahí está el problema
-                    corners, ids, rejected = cv2.aruco.detectMarkers(
-                        gray, 
-                        self.aruco_dict, 
-                        parameters=self.aruco_params
-                    )
-                    logger.debug(f"Marcadores detectados: {len(ids) if ids is not None else 0}")
-                    # --- TRUCO DE DEBUG ---
-
-                    # Dibuja en ROJO los cuadros que el algoritmo VIÓ pero DESCARTÓ por no ser ArUcos válidos
+                if img_b64:
+                    if "vision" in img_b64 and "{" not in img_b64:
+                        logger.warning(f"[ADVERTENCIA] Posible error en payload de imagen: {img_b64[:30]}...")
+                        self.image_queue.task_done()
+                        continue
+                    # 2. Extraer la imagen en Base64 y decodificarla
+                    # Basado en el payload que envía tu vision_agent.py
+                    img_bytes = base64.b64decode(img_b64)
                     
-                    #cv2.aruco.drawDetectedMarkers(frame, rejected, borderColor=(0, 0, 255))
-                    cv2.aruco.drawDetectedMarkers(frame,corners,ids,borderColor=(0,255,0))
-                    cv2.imshow("debug_window",frame)
-                    cv2.waitKey(1)
-                    if ids is None:
-                        logger.warning("No markers detected on frame")
-                        logger.debug(f"Marcadores rechazados: {len(rejected)}")
-                        self.missed_frames+=1
-                        if self.missed_frames %10 ==0:
-                            logger.warning(f"Ojo {self.missed_frames} frames sin ver robots")
-                    else:
-                        self.missed_frames = 1
-                        ids_flat = ids.flatten()
-                        self.current_frame = frame
-                        self.last_corners = corners
-                        self.last_ids = ids
-                        for i, corner in enumerate(corners):
-                            # 1. Obtener puntos clave del marcador en píxeles (u, v)
-                            c = corner[0] # Esquinas: [0]=atrás-izq, [1]=atrás-der, [2]=alante-der, [3]=alante-izq (aprox)
-                            
-                            # Centro del marcador en píxeles
-                            pixel_center = np.mean(c, axis=0)
-                            
-                            # Punto frontal (media de las dos esquinas delanteras para definir el "morro")
-                            pixel_front = np.mean([c[1], c[2]], axis=0) 
+                    # 3. Convertir bytes a imagen de OpenCV
+                    np_array = np.frombuffer(img_bytes, dtype=np.uint8)
+                    frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
 
-                            # 2. Transformar puntos de Píxeles -> Mundo Real usando la Homografía
-                            # cv2.perspectiveTransform requiere un array de forma (N, 1, 2)
-                            # Creamos un array de float32 con forma (2, 1, 2)
-                            pts = np.array([pixel_center, pixel_front], dtype='float32').reshape(-1, 1, 2)
-                            # 3. Aplicar la transformación
-                            # Si self.H es tu matriz 3x3
-                            try:
-                                real_pts = cv2.perspectiveTransform(pts, self.H)
-                                
-                                # Extraer los resultados (ahora tienen forma 2, 1, 2)
-                                real_x, real_y = real_pts[0][0]
-                                front_x, front_y = real_pts[1][0]
 
-                                # 4. Calcular ángulo
-                                x_raw, y_raw = real_pts[0][0]
-                                fx_raw, fy_raw = real_pts[1][0]
-                                # 2. Re-mapeo al nuevo origen (Esquina inferior derecha)
-                                # Invertimos los ejes restando del máximo
-                                x_new = (x_raw-self.OFFSET_X)*self.SCALE_X
-                                y_new = (y_raw-self.OFFSET_Y)*self.SCALE_Y
+                    # Aplicamos una ecualización de histograma para resaltar los bordes
+                    # Esto ayuda mucho si la iluminación es pobre
+                    #gray = cv2.equalizeHist(gray)
+                    if frame is not None:
+                        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        # 2. DETECCIÓN DE ARUCOS
+                        # corners: lista de esquinas de los marcadores detectados
+                        # ids: identificadores de cada marcador
+                        #Justo antes de detectMarkers, aplica un umbral manual para testear
+                        # Esto te permitirá ver si el ArUco se está "emborronando"
+                        _, testing_thresh = cv2.threshold(gray, 150, 180, cv2.THRESH_BINARY)
+                        #cv2.imshow("Test Umbral", testing_thresh) # Si aquí el ArUco se ve todo negro o todo blanco, ahí está el problema
+                        corners, ids, rejected = cv2.aruco.detectMarkers(
+                            gray, 
+                            self.aruco_dict, 
+                            parameters=self.aruco_params
+                        )
+                        logger.debug(f"Marcadores detectados: {len(ids) if ids is not None else 0}")
+                        # --- TRUCO DE DEBUG ---
+
+                        # Dibuja en ROJO los cuadros que el algoritmo VIÓ pero DESCARTÓ por no ser ArUcos válidos
                         
-                                # 3. Cálculo del Yaw en el nuevo sistema
-                                # Calculamos el frente nuevo también para obtener el vector dirección
-                                fx_new = (fx_raw-self.OFFSET_Y)*self.SCALE_X
-                                fy_new = (fy_raw-self.OFFSET_Y)*self.SCALE_Y
+                        #cv2.aruco.drawDetectedMarkers(frame, rejected, borderColor=(0, 0, 255))
+                        cv2.aruco.drawDetectedMarkers(frame,corners,ids,borderColor=(0,255,0))
+                        cv2.imshow("debug_window",frame)
+                        cv2.waitKey(1)
+                        if ids is None:
+                            logger.warning("No markers detected on frame")
+                            logger.debug(f"Marcadores rechazados: {len(rejected)}")
+                            self.missed_frames+=1
+                            if self.missed_frames %10 ==0:
+                                logger.warning(f"Ojo {self.missed_frames} frames sin ver robots")
+                        else:
+                            self.missed_frames = 1
+                            ids_flat = ids.flatten()
+                            self.current_frame = frame
+                            self.last_corners = corners
+                            self.last_ids = ids
+                            for i, corner in enumerate(corners):
+                                # 1. Obtener puntos clave del marcador en píxeles (u, v)
+                                c = corner[0] # Esquinas: [0]=atrás-izq, [1]=atrás-der, [2]=alante-der, [3]=alante-izq (aprox)
                                 
-                                # 2. Calcular el ángulo en PÍXELES (aquí nunca te dará 0)
-                                # Invertimos el eje Y de la imagen porque en OpenCV crece hacia abajo
-                                dx_px = pixel_front[0] - pixel_center[0]
-                                dy_px = -(pixel_front[1] - pixel_center[1]) 
+                                # Centro del marcador en píxeles
+                                pixel_center = np.mean(c, axis=0)
+                                
+                                # Punto frontal (media de las dos esquinas delanteras para definir el "morro")
+                                pixel_front = np.mean([c[1], c[2]], axis=0) 
 
-                                yaw_new = np.arctan2(dy_px, dx_px)
-                                
-                                logger.info(f"ID {ids[i][0]}: X={x_new:.2f}, Y={y_new:.2f}, Th={yaw_new:.2f}")
-                                # 4. (Opcional) Publicar para el servidor/robots
-                                # 4. ENVÍO DE DATOS
-                                robot_id = int(ids[i][0])
-                                target_topic = f"{robot_id}/pos"
+                                # 2. Transformar puntos de Píxeles -> Mundo Real usando la Homografía
+                                # cv2.perspectiveTransform requiere un array de forma (N, 1, 2)
+                                # Creamos un array de float32 con forma (2, 1, 2)
+                                pts = np.array([pixel_center, pixel_front], dtype='float32').reshape(-1, 1, 2)
+                                # 3. Aplicar la transformación
+                                # Si self.H es tu matriz 3x3
+                                try:
+                                    real_pts = cv2.perspectiveTransform(pts, self.H)
+                                    
+                                    # Extraer los resultados (ahora tienen forma 2, 1, 2)
+                                    real_x, real_y = real_pts[0][0]
+                                    front_x, front_y = real_pts[1][0]
+
+                                    # 4. Calcular ángulo
+                                    x_raw, y_raw = real_pts[0][0]
+                                    fx_raw, fy_raw = real_pts[1][0]
+                                    # 2. Re-mapeo al nuevo origen (Esquina inferior derecha)
+                                    # Invertimos los ejes restando del máximo
+                                    x_new = (x_raw-self.OFFSET_X)*self.SCALE_X
+                                    y_new = (y_raw-self.OFFSET_Y)*self.SCALE_Y
                             
-                                payload = {
-                                    "x": round(float(x_new), 2),
-                                    "y": round(float(y_new), 2),
-                                    "yaw": round(float(yaw_new), 3),
-                                    "timestamp": time.time()
-                                }
-                                self.agent.send(target_topic,payload)
-                                logger.debug(f"Enviado -> {target_topic}: {payload}")
+                                    # 3. Cálculo del Yaw en el nuevo sistema
+                                    # Calculamos el frente nuevo también para obtener el vector dirección
+                                    fx_new = (fx_raw-self.OFFSET_Y)*self.SCALE_X
+                                    fy_new = (fy_raw-self.OFFSET_Y)*self.SCALE_Y
+                                    
+                                    # 2. Calcular el ángulo en PÍXELES (aquí nunca te dará 0)
+                                    # Invertimos el eje Y de la imagen porque en OpenCV crece hacia abajo
+                                    dx_px = pixel_front[0] - pixel_center[0]
+                                    dy_px = -(pixel_front[1] - pixel_center[1]) 
 
-                            except cv2.error as e:
-                                logger.error(f"Error en la transformación: {e}")
+                                    yaw_new = np.arctan2(dy_px, dx_px)
+                                    
+                                    logger.info(f"ID {ids[i][0]}: X={x_new:.2f}, Y={y_new:.2f}, Th={yaw_new:.2f}")
+                                    # 4. (Opcional) Publicar para el servidor/robots
+                                    # 4. ENVÍO DE DATOS
+                                    robot_id = int(ids[i][0])
+                                    target_topic = f"{robot_id}/pos"
+                                
+                                    payload = {
+                                        "x": round(float(x_new), 2),
+                                        "y": round(float(y_new), 2),
+                                        "yaw": round(float(yaw_new), 3),
+                                        "timestamp": time.time()
+                                    }
+                                    try:
+                                        self.agent.send(target_topic,payload)
+                                        logger.debug(f"Enviado -> {target_topic}: {payload}")
+                                    except Exception as send_error:
+                                        logger.error(f"[ERROR] Error al enviar datos a {target_topic}: {send_error}")
+                                except cv2.error as e:
+                                    logger.error(f"Error en la transformación: {e}")
                 self.image_queue.task_done()
             except Exception as e:
                 logger.error(f"[ERROR] Error al procesar frame en hilo aislado: {e}", exc_info=True)
